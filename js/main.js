2179f47f6c5d6d78181e51513eec0d216d553196
sakib <ahmadsakibabdullah@gmail.com>
2019-09-12 14:29:06 +0600

    now i'm complete construction-psd home page-2.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 (function ($) {
   'use strict';
 
+// meanmenu
+
+  jQuery('#mobile-menu').meanmenu({
+    meanMenuContainer: '.mobile-menu',
+    meanScreenWidth: "991"
+  });
+
+
+  $('.info-bar').on('click',function(){
+      $('.extra-info').addClass('info-open');
+  })
+
+  $('.close-icon').on('click',function(){
+    $('.extra-info').removeClass('info-open');
+})
+
   // slider-active
   $('.slider-active').slick({
     dots: false,
@@ -15,7 +31,7 @@
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
@@ -54,7 +70,7 @@
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 991,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2
@@ -90,9 +106,9 @@
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 991,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 2
         }
       },
@@ -107,6 +123,26 @@
     ]
   });
 
+
+  $('.testimonial-item-active').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.testimonial-nav'
+  });
+  $('.testimonial-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.testimonial-item-active',
+    dots: false,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: 0
+
+  });
 
 
   // brand-active
@@ -123,14 +159,14 @@
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
-          dots: true
+          dots: false
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 991,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
+          slidesToShow: 4,
+          slidesToScroll: 1
         }
       },
       {
@@ -154,7 +190,7 @@
     }
   })
 
-  $('.protfolio-menu').on('click', 'button', function () {
+  $('.portfolio-menu').on('click', 'button', function () {
     var filterValue = $(this).attr('data-filter');
     grid.isotope({
       filter: filterValue
